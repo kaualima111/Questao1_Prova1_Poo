d9b1edeeb35b7ce9295bbe36ac5969b4f615f5fd
kaualima111 <158177741+kaualima111@users.noreply.github.com>
2024-09-09 16:29:03 -0300

    Add files via upload

---
+++ Nome/Nome/NomeProprio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class NomeProprio
+{
+    private string nome_completo;
+    private string nome_paper;
+
+    public NomeProprio(string nomeCompleto)
+    {
+        nome_completo = nomeCompleto;
+        nome_paper = GerarNomePaper(nome_completo);
+    }
+
+    private string GerarNomePaper(string nomeCompleto)
+    {
+        string[] partes = nomeCompleto.Split(' ');
+
+        if (partes.Length == 0)
+        {
+            return "";
+        }
+
+        string sobrenome = partes[partes.Length - 1];
+
+        string primeiroNome = partes.Length > 1 ? partes[0] : "";
+
+        string inicialNomeDoMeio = partes.Length > 2 ? partes[1][0] + "." : "";
+
+        if (!string.IsNullOrEmpty(inicialNomeDoMeio))
+        {
+            return $"{sobrenome}, {primeiroNome} {inicialNomeDoMeio}";
+        }
+        else
+        {
+            return $"{sobrenome}, {primeiroNome}";
+        }
+    }
+
+    public void ImprimeNomePaper()
+    {
+        Console.WriteLine(nome_paper);
+    }
+}
---
+++ Nome/Nome/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Kauã Cordeiro Durães de Lima - 12311ECP017
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Digite o nome completo: ");
+        string nomeCompleto = Console.ReadLine();
+
+        NomeProprio nome = new NomeProprio(nomeCompleto);
+
+        nome.ImprimeNomePaper();
+
+        Console.ReadLine();
+    }
+}
